4bf02e2682d26155841e6274273e2b1ec3f64085
Connor Blandin <cblandin@charter.net>
2019-08-21 22:17:45 -0400

    Preliminary Traffic Light Detector implementation using built in light states

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -7,6 +7,7 @@ from styx_msgs.msg import Lane
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from light_classification.tl_classifier import TLClassifier
+from scipy.spatial import KDTree
 import tf
 import cv2
 import yaml
@@ -48,6 +49,8 @@ class TLDetector(object):
         self.last_state = TrafficLight.UNKNOWN
         self.last_wp = -1
         self.state_count = 0
+        
+        self.waypoint_tree = None
 
         rospy.spin()
 
@@ -56,6 +59,9 @@ class TLDetector(object):
 
     def waypoints_cb(self, waypoints):
         self.waypoints = waypoints
+        if not self.waypoint_tree:
+            #self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree([[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints])
 
     def traffic_cb(self, msg):
         self.lights = msg.lights
@@ -101,7 +107,7 @@ class TLDetector(object):
 
         """
         #TODO implement
-        return 0
+        return self.waypoint_tree.query([x,y],1)[1]
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -113,7 +119,7 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        if(not self.has_image):
+        """if(not self.has_image):
             self.prev_light_loc = None
             return False
 
@@ -121,7 +127,9 @@ class TLDetector(object):
 
         #Get classification
         return self.light_classifier.get_classification(cv_image)
-
+        """
+        return light.state
+        
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
             location and color
@@ -131,18 +139,28 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        light = None
-
+        closest_light = None
+        line_wp_ndx = None
+        
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
         if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose)
+            car_wp_ndx = self.get_closest_waypoint(self.pose.pose)
 
-        #TODO find the closest visible traffic light (if one exists)
+            #TODO find the closest visible traffic light (if one exists)
+            diff = len(self.waypoints.waypoints)
+            for i, light in enumerate(self.lights):
+                line = stop_line_positions[i]  #Get stop line waypoint index
+                temp_wp_ndx = self.get_closest_waypoint(line[0],line[1])
+                delta_ndx = temp_wp_ndx - car_wp_ndx
+                if delta_ndx >= 0 and delta_ndx < diff: #if it is closer and in front of car, update
+                    diff = delta_ndx
+                    closest_light = light
+                    line_wp_ndx = temp_wp_ndx
 
         if light:
             state = self.get_light_state(light)
-            return light_wp, state
+            return line_wp_ndx, state
         self.waypoints = None
         return -1, TrafficLight.UNKNOWN
 
